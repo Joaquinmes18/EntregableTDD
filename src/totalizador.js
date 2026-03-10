8d77eced5b0664175bb7ece127ef258cbd9e7312
Rubén Cordero <garu1812@gmail.com>
2026-03-09 21:34:17 -0400

    feat: calcular impuesto según el estado con CA por defecto

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,4 +1,7 @@
-function calcularTotal(cantidad, precio) {
-  return cantidad * precio;
+function calcularTotal(cantidad, precio, estado = 'CA') {
+  const impuestos = { 'UT': 0.0665, 'NV': 0.08, 'TX': 0.0625, 'AL': 0.04, 'CA': 0.0825 };
+  const neto = cantidad * precio;
+  const impuestoAplicado = neto * (impuestos[estado] || 0);
+  return Number((neto + impuestoAplicado).toFixed(2));
 }
 module.exports = calcularTotal;
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -2,6 +2,10 @@ const calcularTotal = require('./totalizador');
 
 describe("Totalizador de Ventas", () => {
   it("deberia calcular el precio neto multiplicando la cantidad por el precio", () => {
-    expect(calcularTotal(20, 3)).toEqual(60);
+    expect(calcularTotal(20, 3, 'UT')).toEqual(63.99); 
+  });
+
+  it("deberia aplicar el impuesto de California (8.25%) por defecto", () => {
+    expect(calcularTotal(20, 3)).toEqual(64.95); 
   });
 });
